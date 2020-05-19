52fc3c306c7ed89d56a9e3425a046ad2c4704fae
cwx93 <76253332@qq.com>
2020-05-19 11:22:02 +0800

    常量定义

---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+export const SUPPLIER_FILTER = [
+  { value: '5', label: '淘宝' },
+  { value: '7', label: '京东' },
+  { value: '11', label: '拼多多' },
+  { value: '14', label: '唯品会' },
+  { value: '21', label: '苏宁' }
+]
