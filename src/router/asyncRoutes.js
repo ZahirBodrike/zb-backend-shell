4683234827c481a2ad94f92b7844ec09fdd397f6
zhangbin <471623510@qq.com>
2020-05-25 18:28:32 +0800

    router优化

--- src/router/asyncRoutes.js
+++ src/router/asyncRoutes.js
@@ -1,37 +1,15 @@
-import componentsRouter from './modules/components'
-import userMngRouter from './modules/userMng'
-import taobaoGoodRouter from './modules/taobaoGood'
-import jingdongGoodRouter from './modules/jingdongGood'
-import pddGoodRouter from './modules/pinduoduoGood'
-import weipinhuiGoodRouter from './modules/weipinhuiGood'
-import suNingGoodRouter from './modules/suNingGood'
-import orderRouter from './modules/orderMng'
-import addOilMngRouter from './modules/addOilMng'
-import materialRouter from './modules/material'
-import operationMngRouter from './modules/operationMng'
-import couponMngRouter from './modules/couponMng'
-import financeMngRouter from './modules/finance'
-import signToMoneyRouter from './modules/signToMoney'
-import redEnvelopeWarRouter from './modules/redEnvelope'
-import rebateMngRouter from './modules/rebateMng'
+const routerModules = require.context('./modules', true, /\.js$/)
+
+const router = []
+
+routerModules.keys().forEach(key => {
+  router.push(routerModules(key).default)
+})
+
+router.sort((a, b) => a.sort ? (a.sort - b.sort) : -1)
 
 const asyncRoutes = [
-  componentsRouter,
-  userMngRouter,
-  taobaoGoodRouter,
-  jingdongGoodRouter,
-  pddGoodRouter,
-  weipinhuiGoodRouter,
-  suNingGoodRouter,
-  orderRouter,
-  addOilMngRouter,
-  operationMngRouter,
-  materialRouter,
-  couponMngRouter,
-  financeMngRouter,
-  signToMoneyRouter,
-  redEnvelopeWarRouter,
-  rebateMngRouter,
+  ...router,
 
   { path: '*', redirect: '/404', hidden: true }
 ]
