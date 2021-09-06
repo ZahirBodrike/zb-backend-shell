669e6802b727f266b70f6e5873f3ccbe89980aef
ZahirBodrike <56261634+ZahirBodrike@users.noreply.github.com>
2021-09-06 17:23:05 +0800

    fix: change path

--- ali-oss.config.js
+++ ali-oss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   url: 'http://box-fe-service.XXX.com/compress',
   assetsPath: '/dist/static/', // 需要上传的静态资源目录，相对于根目录
-  remotePath: '/elephant-add-oil-mng-admin/static/' // 需要上传的阿里 oss 远程目录，相对于 bucket 路径
+  remotePath: '/static/' // 需要上传的阿里 oss 远程目录，相对于 bucket 路径
 }
